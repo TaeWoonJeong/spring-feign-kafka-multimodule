fb0dc394ea997011661cd1637069251f1018c56d
TaeWoonJeong <pig07911@naver.com>
2023-04-05 23:43:08 +0900

    test

--- module1/src/main/java/com/jose/module1/controller/Module1Controller.java
+++ module1/src/main/java/com/jose/module1/controller/Module1Controller.java
@@ -23,7 +23,7 @@ public class Module1Controller {
         this.environment = environment;
         this.module1Service = module1Service;
     }
-
+//
     @GetMapping("/info")
     public String getModule1Info() {
         String port = environment.getProperty("local.server.port");

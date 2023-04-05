41219d6a079b81df7606119adcf1ccbb57c9345a
정태운 <50254500+TaeWoonJeong@users.noreply.github.com>
2023-04-06 08:51:47 +0900

    Update Module2.java

--- module2/src/main/java/com/jose/module2/Module2.java
+++ module2/src/main/java/com/jose/module2/Module2.java
@@ -3,6 +3,7 @@ package com.jose.module2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Module2 {
 

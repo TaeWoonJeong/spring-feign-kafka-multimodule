4e79b4ab00caee6aa3a95b87734ab541a73024f5
TaeWoonJeong <pig07911@naver.com>
2023-04-09 13:28:40 +0900

    consumer kakfa-topic modify

--- consumer/src/main/java/com/jose/consumer/listener/MessageConsumerListener.java
+++ consumer/src/main/java/com/jose/consumer/listener/MessageConsumerListener.java
@@ -9,14 +9,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumerListener {
-
     private final MessageConsumerService messageConsumerService;
 
     public MessageConsumerListener(MessageConsumerService messageConsumerService) {
         this.messageConsumerService = messageConsumerService;
     }
 
-    @KafkaListener(topics="my-message", groupId = "test-consumer-group1", containerFactory = "customContainerFactory")
+    @KafkaListener(topics="${app.topic}", groupId = "test-consumer-group1", containerFactory = "customContainerFactory")
     public void recordListener(ConsumerRecord<String, String> record) {
         Gson gson = new Gson();
         GetMessageResponseDto getMessage = gson.fromJson(record.value(), GetMessageResponseDto.class);

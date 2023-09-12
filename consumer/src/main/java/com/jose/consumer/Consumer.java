c25710a8512802726d14544a4bd33b5da67f0b5e
TaeWoonJeong <pig07911@naver.com>
2023-09-12 22:31:34 +0900

    auto sink

--- consumer/src/main/java/com/jose/consumer/Consumer.java
+++ consumer/src/main/java/com/jose/consumer/Consumer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Consumer {
+    
     public static void main(String[] args) {
         SpringApplication.run(Consumer.class, args);
     }
--- producer/src/main/java/com/jose/producer/Producer.java
+++ producer/src/main/java/com/jose/producer/Producer.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class Producer {
+
     public static void main(String[] args) {
         SpringApplication.run(Producer.class, args);
     }
